ee3757b2833abbe423e53950001d54ba8fbb61cc
Charlie B <minaboerdijk@gmail.com>
2025-03-12 20:33:28 +0100

    Revert "deleted old shit"
    This reverts commit 4dbfcab39e1d5f597798cf463527e68cca708b92.

---
+++ OrganismeClasses 3.0/Organism.cs
@@ -0,0 +1,48 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganismClasses
+{
+    [Table("Organism")]
+    public class Organism
+    {
+
+        [PrimaryKey, AutoIncrement]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        public enum Types
+        {
+            Animal,
+            Plant
+        }
+
+        /*public enum Origins
+        {
+            Native,
+            Foreign
+        }*/
+
+
+        [Column("Name")]
+        public string Name { get; set; } = "DefaultName";
+
+        [Column("Type")]
+        public Types Type { get; set; } = Types.Animal;
+
+        [Column("Origin")]
+        public string Origin { get; set; } = "DefaultOrigin";
+
+        public virtual string DryDescription()
+        {
+            //miss type eruit laten. Alleen naam.
+            return $"Name:\t\t{Name}";
+        }
+
+    }
+
+}
